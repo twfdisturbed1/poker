3d40bf704e927c945f4ca4c39da6e6467e8fc46a
twfdisturbed1 <williamcebertmullins@gmail.com>
2018-11-12 18:11:31 -0500

    Removed files

--- Web.rb
+++
@@ -1,7 +0,0 @@
-require "open-uri"
-
-remote_base_url = "http://en.wikipedia.org/wiki"
-remote_page_name = "Ruby"
-remote_full_url = remote_base_url + "/" + remote_page_name
-
-puts open(remote_full_url).read
--- cards.rb
+++
@@ -1,2 +0,0 @@
-cards = {"2c" => '<img width="115" height="168" src="/images/card/2_of_clubs.png">',
-       "3c" => '<img width="115" height="168" src="/images/card/3_of_clubs.png">'}
